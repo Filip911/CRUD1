8dbc78bcba9f375190e5dc74cd4b7741e5b0c496
Filip911 <filipworkrate@gmail.com>
2019-12-19 10:37:46 +0100

    Half way through

--- index.php
+++ index.php
@@ -112,7 +112,7 @@
                     <td><?= $row['phone']; ?></td>
                     <td>
                        <a href="details.php?details=<?= $row['id']; ?>" class="badge badge-primary p-2">Details</a> 
-                       <a href="action.php?delete=<?= $row['id']; ?>" class="badge badge-danger p-2">Delete</a> 
+                       <a href="action.php?delete=<?= $row['id']; ?>" class="badge badge-danger p-2" onclick="return confirm('Are you sure you wanna delete this record?')">Delete</a> 
                        <a href="index.php?edit=<?= $row['id']; ?>" class="badge badge-success p-2">Edit</a> 
                     </td>
                 </tr>    
